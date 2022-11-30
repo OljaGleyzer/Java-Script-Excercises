a1679fff7edabb087c023283d6c5e294268617de
OljaGleyzer <o.gleyzer@gmail.com>
2022-11-30 17:23:26 +0100

    finished excercise 15

--- main.js
+++ main.js
@@ -65,30 +65,48 @@ for (let j = 0; j < even.length; j++) {
 
 
 let arrNum = [3,4,5,6]
-let lowestNum = Math.min(...arrNum);
-console.log(`The lowest number is: ${lowestNum}`); 
+let ageMax = 100
+// 1st solution
+// let lowestNum = Math.min(...arrNum);
+// console.log(`The lowest number is: ${lowestNum}`); 
 
-function lowestNumber(arrNum) {
- 
- if (lowestNum); {console.log(`Number ${lowestNum} `);
- }
- 
+//Solution 2
+
+function eight(){
+    
+for (let index = 0; index < arrNum.length; index++) {
+    if (arrNum[index]< ageMax) {ageMax = arrNum[index]}   
 }
-lowestNumber(arrNum);
+}
+eight();
+console.log(ageMax);
+
  
 //Excercise 9
 // Write a function that receives an array as a parameter and prints the biggest number in the array to the console. Save the changes to your JavaScript file. Reload the HTML page in your browser. You should see the biggest number in the array printed in the console. If not, investigate and fix it.
-let arrNum2 = [4,5,6]
-let highestNum = Math.max(...arrNum);
-console.log(`The highest number is: ${highestNum}`); 
 
-function highestNumber(arrNum) {
+let arrNum2 = [4,5,6,9]
+let ageMin = 1
+//Solution 1
+//let highestNum = Math.max(...arrNum);
+//console.log(`The highest number is: ${highestNum}`); 
+
+// function highestNumber(arrNum) {
  
- if (highestNum); {console.log(`Number ${highestNum} `);
- }
+//  if (highestNum); {console.log(`Number ${highestNum} `);
+//  }
  
-}
-highestNumber(arrNum2);
+// }
+// highestNumber(arrNum2);
+
+function nine(){
+    
+    for (let i = 0; i <= arrNum2.length; i++) {
+        if (arrNum2[i]> ageMin) {ageMin = arrNum2[i]}   
+    }
+    }
+    nine();
+    console.log(ageMin);
 
 //Excercise 10
 // Write a function that receives two parameters, an array, and an index. The function will print the value of the element at the given position (one-based) to the console. For example, given the following array and index, the function will print '6'.
@@ -110,8 +128,21 @@ index(array);
 // var array = [3,6,67,6,23,11,100,8,93,0,17,24,7,1,33,45,28,33,23,12,99,100];
 
 // Save the changes to your JavaScript file. Reload the HTML page in your browser. You should see an array of the repeated numbers printed in the console. If not, investigate and fix it.
+ 
+const numArray = [1,2,3,4]
 
+for (let counterA = 0; counterA < numArray.length; counterA++) {
+    console.log("first loop", counterA);
+    
 
+    for (let counterB = 0; counterB < numArray.length; counterB++) {
+        console.log("second loop", counterB)
+        console.log(numArray[counterB]) 
+        
+    }
+}
+
+//
 
 // Excercise 12
 // Write a simple JavaScript function to join all elements of the following array into a string.
@@ -124,4 +155,66 @@ index(array);
 
 // If not, investigate and fix it.
 
-myColor = ["Red", "Green", "White", "Black"];
+ let myColor = ["Red", "Green", "White", "Black"];
+let text = myColor.join();
+
+
+console.log(text);
+
+//Java Script String
+// Excercise 13
+// Write a JavaScript function that reverses a number. For example, if x = 32443 then the output should be 34423. Save your JavaScript file and reload the page. Make sure you see the correct output. If not, investigate and fix it.
+
+function simpleString (){
+    let x = "32443";
+    console.log("this is a string of "+ x)
+}
+simpleString();
+
+//Excercise 14
+//Write a JavaScript function that returns a string in alphabetical order. For example, if x = 'webmaster' then the output should be 'abeemrstw'. Punctuation and numbers aren't passed in the string. Save your JavaScript file and reload the page. Make sure you see the correct output. If not, investigate and fix it.
+
+// Loop through the string and convert every char into number
+let word = "webmaster";
+let singlechar = word.split("");
+console.log(singlechar);
+
+let sortet = singlechar.sort();
+console.log(sortet);
+let stringagain= sortet.toString();
+console.log(stringagain);
+let final= stringagain.replaceAll(",", "");
+console.log(final);
+
+//Excercise 15
+//Write a JavaScript function that converts the first letter of every word to uppercase. For example, if x = "prince of persia" then the output should be "Prince Of Persia". Save your JavaScript file and reload the page. Make sure you see the correct output. If not, investigate and fix it.
+
+
+
+//1st try
+//let sentence = "prince of persia";
+//let splitwords= sentence.split("");
+// let uppercase = splitwords.toUpperCase(); // returns every letter capital
+// console.log(uppercase);
+
+// To do:
+// split the sentence into an array
+// loop trough the array of three words
+// each time use toUpperCase() method on the forst character
+// join the words 
+
+function capitalizeFirstLetters(phrase) {
+    return phrase
+      .toLowerCase()
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  }
+  console.log (capitalizeFirstLetters("prince of persia"));
+  
+  //Excercise 16
+ //Write a JavaScript function that finds the longest word in a phrase. For example, if x = "Web Development Tutorial", then the output should be "Development". Save your JavaScript file and reload the page. Make sure you see the correct output. If not, investigate and fix it.
+ //
+ 
+  
+  
